25c37030fcf6f47768671891359d6c59065ccb81
Rafal_Slomka <rafal_slomka@poczta.onet.pl>
2017-06-06 12:07:36 +0200

    rozgrzebany javascript

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
-var question = [["The usual width of parapet walls along Highways in hilly region, is", "50 cm", "60 cm", "70 cm", "100 cm", 2],
-               ["Design of horizontal and vertical alignments, super-elevation, sight distance and grades, is worst affected by", "width of the vehicle", "length of the vehicle", "height of the vehicle", "speed of the vehicle", 4],
-               ["Thickness of a pavement may be reduced considerably by", "compaction of soil","stabilisation of soil",  "drainage of soil", "combination of all the above.", 4],
-               ["For the movement of vehicles at an intersection of two roads, without any interference, the type of grade separator generally preferred to, is",  "delta", "trumpet", "diamond interchange", "clover leaf.", 4],
-               ["Road makers along roads from the edge of a kerb should not be less than", "40 cm", "45 cm", "60 cm", "55 cm", 3]];
+var question = [["Jak przeglądasz stronę internetową na co zwraczasz uwagę w pierszej kolejności", "obrazki", "układ strony", "szata graficzna", "efekty specjalne", -50, +50, -25, +25],
+               ["Jeśli kupowałbyś samochód na jakie rzeczy zwróciłbyś uwagę w pierwszej kolejności", "niezawodność", "wygląd", "osiągi", "szybkość", +50, +25, -25, 0],
+               ["Czy lubisz się uczyć języków obcych", "tak", "raczej tak",  "raczej nie", "zdecydowanie nie", +50, +25, -25, 0],
+               ["Jaki zestaw umiejętności Cie najtrafniej określa",  "pracowity i dokładny", "kreatywny i chłonny nowych umiejętności", "ostrożny i podejrzliwy", "wzrokowiec i chłonny nowych wrażeń", +75, -50, +50, -100],
+               ["Gdzie byś najchętniej spędził czas wolny", "w interaktywnym laboratorium technologii", "w galerii sztuki", "na łonie natury", "w krzemowej dolinie", +100, -100, 0, +50]];
 
 
 var score = 0;
 var quesNum = 0;
 alert('pod zmiennymi');
+
 function displayQuiz(){
 
 var quest = document.getElementById("question");
@@ -25,7 +26,7 @@ quest.innerHTML = question[quesNum][0];
     displayQuiz();
 })();
 
-  function validat(item){
+  function validate(item){
        
        if(item === "choice"+question[quesNum][5]){
 
@@ -62,4 +63,3 @@ function listenMe(e) {
     if(quesNum < 5){
     displayQuiz();}
 }
-`
