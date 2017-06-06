67becd1735a6949a8f9eceeec6a798b397d31d4d
Rafal_Slomka <rafal_slomka@poczta.onet.pl>
2017-06-06 11:18:52 +0200

    podpiety css i js

---
+++ quiz.js
@@ -0,0 +1,9 @@
+var question = [["The usual width of parapet walls along Highways in hilly region, is", "50 cm", "60 cm", "70 cm", "100 cm", 2],
+               ["Design of horizontal and vertical alignments, super-elevation, sight distance and grades, is worst affected by", "width of the vehicle", "length of the vehicle", "height of the vehicle", "speed of the vehicle", 4],
+               ["Thickness of a pavement may be reduced considerably by", "compaction of soil","stabilisation of soil",  "drainage of soil", "combination of all the above.", 4],
+               ["For the movement of vehicles at an intersection of two roads, without any interference, the type of grade separator generally preferred to, is",  "delta", "trumpet", "diamond interchange", "clover leaf.", 4],
+               ["Road makers along roads from the edge of a kerb should not be less than", "40 cm", "45 cm", "60 cm", "55 cm", 3]];
+
+
+var score = 0;
+var quesNum = 0;
