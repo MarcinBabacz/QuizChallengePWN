456e70fa61b5dc862e4b085f4b7f6593bff30467
Marcin <babacz.marcin@gmail.com>
2017-06-06 22:57:02 +0200

    Odwołanie zapisz/ poprawna ilośc pytać jvascript

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-var question = [["Jak przeglądasz stronę internetową na co zwraczasz uwagę w pierszej kolejności", "obrazki", "układ strony", "szata graficzna", "efekty specjalne", -50, +50, -25, +25],
-               ["Jeśli kupowałbyś samochód na jakie rzeczy zwróciłbyś uwagę w pierwszej kolejności", "niezawodność", "wygląd", "osiągi", "szybkość", +100, +25, -25, 0],
-               ["Czy lubisz się uczyć języków obcych", "tak", "raczej tak",  "raczej nie", "zdecydowanie nie", +50, +25, -25, 0],
-               ["Jaki zestaw umiejętności Cie najtrafniej określa",  "pracowity i dokładny", "kreatywny i chłonny nowych umiejętności", "ostrożny i podejrzliwy", "wzrokowiec i chłonny nowych wrażeń", +75, -50, +50, -100],
-               ["Gdzie byś najchętniej spędził czas wolny", "w interaktywnym laboratorium technologii", "w galerii sztuki", "na łonie natury", "w krzemowej dolinie", +100, -100, 0, +50]];
+var question = [["Jak przeglądasz stronę internetową na co zwraczasz uwagę w pierszej kolejności", "Obrazki", "Układ strony", "Szata graficzna", "Sfekty specjalne", -50, +50, -25, +25],
+               ["Jeśli kupowałbyś samochód na jakie rzeczy zwróciłbyś uwagę w pierwszej kolejności", "Niezawodność", "Wygląd", "Osiągi", "Szybkość", +100, +25, -25, 0],
+               ["Czy lubisz się uczyć języków obcych", "Tak", "Raczej tak",  "Raczej nie", "Zdecydowanie nie", +50, +25, -25, 0],
+               ["Jaki zestaw umiejętności Cie najtrafniej określa",  "Pracowity i dokładny", "Kreatywny i chłonny nowych umiejętności", "Ostrożny i podejrzliwy", "Wzrokowiec i chłonny nowych wrażeń", +75, -50, +50, -100],
+               ["Gdzie byś najchętniej spędził czas wolny", "W interaktywnym laboratorium technologii", "W galerii sztuki", "Na łonie natury", "W krzemowej dolinie", +100, -100, 0, +50]];
 
 
 var score = 0;
@@ -18,6 +18,10 @@ function displayQuiz(){
 
 var quest = document.getElementById("question");
 quest.innerHTML = question[quesNum][0];
+
+        
+        var progress = document.getElementById("progress");
+	progress.innerHTML = "<p>" + "Pytanie " + (quesNum+1) + " z " + questNumber + ".</p>";
 
   for(j=1; j<=4; j++){
      var opt = document.getElementById("choice"+j);
@@ -40,34 +44,32 @@ quest.innerHTML = question[quesNum][0];
 	else if (item === "d"){
        	score += question[quesNum][8];
 	}
-         if(quesNum  === questNumber - 1){
+         if(quesNum  === questNumber-1){
         	var mainBody = document.getElementById("quiz");
 			if (score < (-questNumber * 100 * 1 / 4)){
-				mainBody.innerHTML = "<h1>GRATULUJE!! jesteś: Frontend</h1>"+
+				mainBody.innerHTML = "<h1>Gratulacje! Twój profil to Frontend</h1>"+
 				"<h2>"+"Twój zmysł artystyczny wskazuje na to, że świetnie odnajdziesz się we frontendzie."+ "</h2>"+
-				"<h2>"+"<imag src=https://reaktor.pwn.pl/wp-content/uploads/2016/12/icons_slash_tech_slash_css-2x.png>"+ "</h2>"+
-				"<div id=\"mail\"><a href=section-content>Zapisz się !</a></div>";
+				
+				"<div id=\"mail\"><a href='http://reaktor.pwn.pl/kurs/front-end-developer/'>Zapisz się !</a></div>";
 				
 				
 			}
 			else if (score > (questNumber * 100 * 1 / 4)){
-				mainBody.innerHTML = "<h1>GRATULUJE!! jesteś: Backend</h1>"+
+				mainBody.innerHTML = "<h1>Gratulacje! Twój profil to Backend</h1>"+
 				"<h2>"+"Twoja umiejętność analitycznego myślenia predysponują Cię do pracy w backendzie.</h2>"+ 
-				"<h2>"+"<imag src=https://reaktor.pwn.pl/wp-content/uploads/2016/12/icons_slash_tech_slash_css-2x.png>"+ "</h2>"+
-				"<div id=\"mail\">"+"<a href=#section-content>Zapisz się !</a></div>";
+				
+				"<div id=\"mail\">"+"<a href='http://reaktor.pwn.pl/kurs/back-end-developer/'t>Zapisz się !</a></div>";
 			}
 			else{
-				mainBody.innerHTML = "<h1>GRATULUJE!! jesteś: Fullstack</h1>"+
+				mainBody.innerHTML = "<h1>Gratulacje! jesteś: Fullstack</h1>"+
 				"<h2>"+"Posiadasz zdolności predysponująe Cię do pracy jako fullstack developer.<h/2>"+ 
-				"<h2>"+"<imag src=https://reaktor.pwn.pl/wp-content/uploads/2016/12/icons_slash_tech_slash_css-2x.png>"+ "</h2>"+
+			
 				"<div id=\"mail\">"+"<a href= #section-content>Zapisz się !</a></div>";
 			};
 
         }
+		quesNum++;
 
-        quesNum++;
-        var progress = document.getElementById("progress");
-	progress.innerHTML = "<p>" + "Pytanie " + (quesNum + 1) + " z " + questNumber + ".</p>";
      }
 
 
